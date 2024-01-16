f28cae0f937ea04551cdf32c6598ff373eae21fd
TheLadysLoveMe <142649696+TheLadysLoveMe@users.noreply.github.com>
2024-01-15 19:24:02 -0500

    Update games.js

--- public/assets/data/games.js
+++ public/assets/data/games.js
@@ -297,7 +297,7 @@ var games = [
   {
     id: 'slope',
     title: 'Slope',
-    url: 'https://google-opensocial.googleusercontent.com/gadgets/ifr?url=https://cdn.jsdelivr.net/gh/google-co-m/js@main/2slope.xml',
+    url: 'https://www.ehabgames.com/games/1v1lol/index.html',
     image: 'https://play-lh.googleusercontent.com/uJn2i9h7KxYQarC_c3K4qH6o7gLtflFnhD_dN14MNkzHJ1NeNFzCL69jpB5mT0vCoQs'
   },
   {
