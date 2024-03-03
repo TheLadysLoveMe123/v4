e4440152ee4e850b3cb9e0aa10d14eb47b5b4e30
TheLadysLoveMe <142649696+TheLadysLoveMe@users.noreply.github.com>
2024-03-03 00:54:41 -0500

    Update games.js

--- public/assets/data/games.js
+++ public/assets/data/games.js
@@ -33,13 +33,13 @@ var games = [
     title: 'Soundboard',
     url: 'https://www.myinstants.com/categories/sound%20effects/',
     image: 'https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_download_software_1/2x1_NSwitchDS_SoundboardButtonsWithInstantSounds_image1600w.jpg'
-    },
+  },
   {  
     id: 'jetpackjoyride',
     title: 'Jetpack Joyride',
     url: 'https://phantomgames.dev/game/jetpack-joyride/index.html',
     image: 'https://raw.githubusercontent.com/3kh0/3kh0-assets/main/eggycar/eggy-car.png'
-   },
+  },
   {
     id: 'bobtherobber2',
     title: 'Bob The Robber 2',
